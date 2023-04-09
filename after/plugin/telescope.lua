2e8acb38e4301c41879984d1e24faa48756cdc33
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2023-04-10 00:40:06 +0200

    FZF and dropdown theme

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,17 +3,42 @@ require("telescope").setup({
     pickers = {
         find_files = {
             theme = "dropdown",
+        },
+        git_files = {
+            theme = "dropdown"
+        },
+        git_branches = {
+            theme = "dropdown",
+        },
+        live_grep = {
+            theme = "dropdown",
+        },
+        lsp_references = {
+            theme = "dropdown",
+        },
+        lsp_dynamic_workspace_symbols = {
+            theme = "dropdown",
+        },
+        diagnostics = {
+            theme = "dropdown",
+        },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
         }
     },
-    extensions = {},
 })
+require("telescope").load_extension("fzf")
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>fF', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-vim.keymap.set('n', 'gr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>fW', function()
     require("telescope.builtin").live_grep {
         additionla_args = function(args)
@@ -21,3 +46,7 @@ vim.keymap.set('n', '<leader>fW', function()
         end
     }
 end)
+vim.keymap.set('n', 'gr', builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>lR", builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>lw", builtin.lsp_dynamic_workspace_symbols, {})
+vim.keymap.set("n", "<leader>lD", builtin.diagnostics, {})
--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -6,7 +6,10 @@ return require("packer").startup(function(use)
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        requires = {
+            { 'nvim-lua/plenary.nvim' },
+            { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+        }
     }
     use({
         'decaycs/decay.nvim',

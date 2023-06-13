4e870425c62a08d46881453ccb49c444ad84766b
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2023-06-13 11:23:04 +0200

    add: some python lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,17 @@ lsp.preset("recommended")
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').pylsp.setup({
+    settings = {
+        pylsp = {
+            plugins = {
+                jedi = {
+                    enviroment = "/usr/bin/python"
+                }
+            }
+        }
+    }
+})
 
 lsp.ensure_installed({
 	"rust_analyzer",

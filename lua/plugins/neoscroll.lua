8bbdb760c0a792dd6a96291513d1cbd4df890cc2
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2023-11-10 23:51:07 +0100

    lazy: neoscroll keymap

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,13 +1,22 @@
 return {
     'karb94/neoscroll.nvim',
-    options = function()
+    lazy = false,
+    config = function()
         require('neoscroll').setup({
-            mappings = { '<C-u>', '<C-d>' },
+            -- mappings = { '<C-u>', '<C-d>' },
+            mappings = {},
             hide_cursor = false,
             stop_eof = true,
             respect_scrolloff = false,
             cursor_scrolls_alone = true,
             performance_mode = true,
+            pre_hook = function() end,
+            post_hook = function() end,
         })
-    end
+        local t = {}
+        t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
+        t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
+        require("neoscroll.config").set_mappings(t)
+    end,
+    keys = {}
 }

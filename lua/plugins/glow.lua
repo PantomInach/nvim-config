a0c17f3f00cecdd3910146d3e1528694fa4c645e
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2025-02-15 22:45:03 +0100

    feat: removed redundant glow bind

--- lua/plugins/glow.lua
+++ lua/plugins/glow.lua
@@ -1,9 +1,7 @@
 return {
     'ellisonleao/glow.nvim',
     config = function()
-        require("glow").setup({
-            vim.keymap.set("n", "<leader>G", ":Glow<CR>")
-        })
+        require("glow").setup()
     end,
     keys = {
         { "<leader>G", ":Glow<CR>" }

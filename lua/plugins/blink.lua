1c1ea1822a0ab7ff9d3315b901e701fd0ac24505
Aaron Neugebauer <72529530+PantomInach@users.noreply.github.com>
2025-05-05 17:25:36 +0200

    Nvim lspconfig (#5)
    * blink: replace recommendations system from nvim-cmp to blink.cmp
    
    * nvim-lspconfig: moved from lsp-zero to nvim-lspconfig
    
    * readme: added nvim-lspconfig
    
    * mason: removed files since they are handeled in nvim-lspconfig file
    
    ---------
    
    Co-authored-by: Aaron Neugebauer <aaron.neugebauer@stud-mail.uni-wuerzburg.de>

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,97 @@
+return {
+    'saghen/blink.cmp',
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        'mikavilpas/blink-ripgrep.nvim',
+    },
+
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+        -- 'super-tab' for mappings similar to vscode (tab to accept)
+        -- 'enter' for enter to accept
+        -- 'none' for no mappings
+        --
+        -- All presets have the following mappings:
+        -- C-space: Open menu or open docs if already open
+        -- C-n/C-p or Up/Down: Select next/previous item
+        -- C-e: Hide menu
+        -- C-k: Toggle signature help (if signature.enabled = true)
+        --
+        -- See :h blink-cmp-config-keymap for defining your own keymap
+        keymap = {
+            preset = 'none',
+            ['<Up>'] = { 'select_prev', 'fallback' },
+            ['<Down>'] = { 'select_next', 'fallback' },
+            ['<CR>'] = { 'accept', 'fallback' },
+            ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+        },
+
+        appearance = {
+            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- Adjusts spacing to ensure icons are aligned
+            nerd_font_variant = 'mono'
+        },
+
+        -- (Default) Only show the documentation popup when manually triggered
+        completion = { documentation = { auto_show = false, auto_show_delay_ms = 50 } },
+
+        signature = { enabled = true, window = { show_documentation = false } },
+
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep' },
+            providers = {
+                ripgrep = {
+                    module = "blink-ripgrep",
+                    name = "Ripgrep",
+                    ---@module "blink-ripgrep"
+                    ---@type blink-ripgrep.Options
+                    opts = {
+                        prefix_min_len = 3,
+                        context_size = 5,
+                        max_filesize = "1M",
+                        project_root_marker = ".git",
+                        project_root_fallback = true,
+                        -- available options ripgrep supports, but you can try
+                        -- "--case-sensitive" or "--smart-case".
+                        search_casing = "--ignore-case",
+                        additional_rg_options = {},
+                        fallback_to_regex_highlighting = true,
+                        ignore_paths = {},
+                        additional_paths = {},
+                        toggles = {
+                            -- The keymap to toggle the plugin on and off from blink
+                            -- completion results. Example: "<leader>tg"
+                            on_off = nil,
+                        },
+                        future_features = {
+                            backend = {
+                                -- The backend to use for searching. Defaults to "ripgrep".
+                                -- Available options:
+                                -- - "ripgrep", always use ripgrep
+                                -- - "gitgrep", always use git grep
+                                -- - "gitgrep-or-ripgrep", use git grep if possible, otherwise
+                                --   ripgrep
+                                use = "ripgrep",
+                            },
+                        },
+                        debug = false,
+                    },
+                }
+            },
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+}
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,127 +1,128 @@
-return {
-    'VonHeikemen/lsp-zero.nvim',
-    lazy = false,
-    branch = "v2.x",
-    dependencies = {
-        { 'neovim/nvim-lspconfig' },
-        {
-            'williamboman/mason.nvim',
-            build = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end
-        },
-        { 'williamboman/mason-lspconfig.nvim' },
-        { 'hrsh7th/nvim-cmp' },
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'L3MON4D3/LuaSnip' },
-        { 'simrat39/rust-tools.nvim' },
-        { 'vigoux/ltex-ls.nvim' },
-    },
-    config = function()
-        local lsp = require('lsp-zero').preset('recommended')
-
-        local cmp = require('cmp')
-        local cmp_action = require('lsp-zero').cmp_action()
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        local border_opts = {
-            border = "rounded",
-            winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-        }
-
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-                -- `Enter` key to confirm completion
-                ['<CR>'] = cmp.mapping.confirm({ select = false }),
-
-                -- Up and down navigation
-                ["<DOWN>"] = cmp.mapping.select_next_item(cmp_select),
-                ["<UP>"] = cmp.mapping.select_prev_item(cmp_select),
-
-                -- Scroll up and down in the completion documentation
-                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-d>'] = cmp.mapping.scroll_docs(4),
-            }),
-            window = {
-                completion = cmp.config.window.bordered(border_opts),
-                documentation = cmp.config.window.bordered(border_opts),
-            }
-        })
-
-        lsp.preset("recommended")
-
-        lsp.format_on_save({
-            format_opts = {
-                async = false,
-                timeout_ms = 1000,
-            },
-            servers = {
-                ['lua_ls'] = { 'lua' },
-                ['rust_analyzer'] = { 'rust' },
-                -- ['zls'] = { 'zig' },
-            },
-        })
-
-        -- (Optional) Configure lua language server for neovim
-        local lspconfig = require('lspconfig')
-        lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-
-        -- Python LSP setup
-        -- lspconfig.python.setup({
-        lsp.configure('pylsp', {
-            settings = {
-                pylsp = {
-                    plugins = {
-                        black = { enabled = true },
-                        autopep8 = { enabled = false },
-                        yapf = { enabled = false },
-                        pylint = { enabled = true },
-                        ruff = { enabled = false },
-                        pyflakes = { enabled = true },
-                        pycodestyle = {
-                            enabled = true,
-                            ignore = { "E501", "W503" },
-                        },
-                        pylsp_mypy = { enabled = true, report_progress = true, live_mode = false },
-                        jedi_completion = { fuzzy = true },
-                        isort = { enabled = true }
-                    }
-                },
-            }
-        })
-
-        -- Rust LSP setup
-        local rust_tool = require("rust-tools")
-        rust_tool.setup()
-
-        -- Latex LSPs setup
-        lspconfig.texlab.setup({
-            settings = {
-                texlab = {
-                    build = { onSave = true },
-                    forwardSearch = { executable = "zathura", args = { "--synctex-forward", "%l:1:%f", "%p" } },
-                },
-            },
-        })
-        lspconfig.ltex.setup({})
-
-        lspconfig.julials.setup({})
-
-        lsp.setup()
-    end,
-    keys = {
-        -- TexLab
-        { "<leader>mb", "<cmd>TexlabBuild<CR>" },
-        { "<leader>mf", "<cmd>TexlabForward<CR>" },
-        -- diagnostic
-        { "gd",         function() vim.lsp.buf.definition() end, },
-        { "K",          function() vim.lsp.buf.hover() end, },
-        { "<leader>ld", function() vim.diagnostic.open_float() end, },
-        { "<leader>lh", function() vim.diagnostic.goto_prev() end, },
-        { "<leader>ll", function() vim.diagnostic.goto_next() end, },
-        { "<leader>la", function() vim.lsp.buf.code_action() end, },
-        { "<leader>lr", function() vim.lsp.buf.rename() end, },
-        { "<leader>ls", function() vim.lsp.buf.signature_help() end, },
-        { "<leader>lf", function() vim.lsp.buf.format() end, },
-    },
-}
+return {}
+-- return {
+--     'VonHeikemen/lsp-zero.nvim',
+--     lazy = false,
+--     branch = "v2.x",
+--     dependencies = {
+--         { 'neovim/nvim-lspconfig' },
+--         {
+--             'williamboman/mason.nvim',
+--             build = function()
+--                 pcall(vim.cmd, 'MasonUpdate')
+--             end
+--         },
+--         { 'williamboman/mason-lspconfig.nvim' },
+--         -- { 'hrsh7th/nvim-cmp' },
+--         -- { 'hrsh7th/cmp-nvim-lsp' },
+--         { 'L3MON4D3/LuaSnip' },
+--         { 'simrat39/rust-tools.nvim' },
+--         { 'vigoux/ltex-ls.nvim' },
+--     },
+--     config = function()
+--         local lsp = require('lsp-zero').preset('recommended')
+--
+--         -- local cmp = require('cmp')
+--         -- local cmp_action = require('lsp-zero').cmp_action()
+--         -- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+--
+--         local border_opts = {
+--             border = "rounded",
+--             winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+--         }
+--
+--         -- cmp.setup({
+--         --     mapping = cmp.mapping.preset.insert({
+--         --         -- `Enter` key to confirm completion
+--         --         ['<CR>'] = cmp.mapping.confirm({ select = false }),
+--         --
+--         --         -- Up and down navigation
+--         --         ["<DOWN>"] = cmp.mapping.select_next_item(cmp_select),
+--         --         ["<UP>"] = cmp.mapping.select_prev_item(cmp_select),
+--         --
+--         --         -- Scroll up and down in the completion documentation
+--         --         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+--         --         ['<C-d>'] = cmp.mapping.scroll_docs(4),
+--         --     }),
+--         --     window = {
+--         --         completion = cmp.config.window.bordered(border_opts),
+--         --         documentation = cmp.config.window.bordered(border_opts),
+--         --     }
+--         -- })
+--
+--         lsp.preset("recommended")
+--
+--         lsp.format_on_save({
+--             format_opts = {
+--                 async = false,
+--                 timeout_ms = 1000,
+--             },
+--             servers = {
+--                 ['lua_ls'] = { 'lua' },
+--                 ['rust_analyzer'] = { 'rust' },
+--                 -- ['zls'] = { 'zig' },
+--             },
+--         })
+--
+--         -- (Optional) Configure lua language server for neovim
+--         local lspconfig = require('lspconfig')
+--         lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+--
+--         -- Python LSP setup
+--         -- lspconfig.python.setup({
+--         lsp.configure('pylsp', {
+--             settings = {
+--                 pylsp = {
+--                     plugins = {
+--                         black = { enabled = true },
+--                         autopep8 = { enabled = false },
+--                         yapf = { enabled = false },
+--                         pylint = { enabled = true },
+--                         ruff = { enabled = false },
+--                         pyflakes = { enabled = true },
+--                         pycodestyle = {
+--                             enabled = true,
+--                             ignore = { "E501", "W503" },
+--                         },
+--                         pylsp_mypy = { enabled = true, report_progress = true, live_mode = false },
+--                         jedi_completion = { fuzzy = true },
+--                         isort = { enabled = true }
+--                     }
+--                 },
+--             }
+--         })
+--
+--         -- Rust LSP setup
+--         local rust_tool = require("rust-tools")
+--         rust_tool.setup()
+--
+--         -- Latex LSPs setup
+--         lspconfig.texlab.setup({
+--             settings = {
+--                 texlab = {
+--                     build = { onSave = true },
+--                     forwardSearch = { executable = "zathura", args = { "--synctex-forward", "%l:1:%f", "%p" } },
+--                 },
+--             },
+--         })
+--         lspconfig.ltex.setup({})
+--
+--         lspconfig.julials.setup({})
+--
+--         lsp.setup()
+--     end,
+--     keys = {
+--         -- TexLab
+--         { "<leader>mb", "<cmd>TexlabBuild<CR>" },
+--         { "<leader>mf", "<cmd>TexlabForward<CR>" },
+--         -- diagnostic
+--         { "gd",         function() vim.lsp.buf.definition() end, },
+--         { "K",          function() vim.lsp.buf.hover() end, },
+--         { "<leader>ld", function() vim.diagnostic.open_float() end, },
+--         { "<leader>lh", function() vim.diagnostic.goto_prev() end, },
+--         { "<leader>ll", function() vim.diagnostic.goto_next() end, },
+--         { "<leader>la", function() vim.lsp.buf.code_action() end, },
+--         { "<leader>lr", function() vim.lsp.buf.rename() end, },
+--         { "<leader>ls", function() vim.lsp.buf.signature_help() end, },
+--         { "<leader>lf", function() vim.lsp.buf.format() end, },
+--     },
+-- }

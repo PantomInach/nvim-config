df7878aa6adb5c5d714110190ddce34a1642b290
Aaron Neugebauer <aaron@ralf-saalmueller.de>
2026-06-06 18:43:54 +0200

    change: removed blink provider

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,7 +2,7 @@ return {
     'saghen/blink.cmp',
     dependencies = {
         'rafamadriz/friendly-snippets',
-        'mikavilpas/blink-ripgrep.nvim',
+        -- 'mikavilpas/blink-ripgrep.nvim',
     },
     version = '1.*',
     ---@module 'blink.cmp'
@@ -30,36 +30,39 @@ return {
         },
         signature = { enabled = true, window = { show_documentation = true } },
         sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep' },
-            providers = {
-                ripgrep = {
-                    module = "blink-ripgrep",
-                    name = "Ripgrep",
-                    ---@module "blink-ripgrep"
-                    ---@type blink-ripgrep.Options
-                    opts = {
-                        prefix_min_len = 3,
-                        context_size = 5,
-                        max_filesize = "1M",
-                        project_root_marker = ".git",
-                        project_root_fallback = true,
-                        search_casing = "--ignore-case",
-                        additional_rg_options = {},
-                        fallback_to_regex_highlighting = true,
-                        ignore_paths = {},
-                        additional_paths = {},
-                        toggles = {
-                            on_off = nil,
-                        },
-                        future_features = {
-                            backend = {
-                                use = "ripgrep",
-                            },
-                        },
-                        debug = false,
-                    },
-                }
+            default = { 'lsp', 'path', 'snippets',
+                -- 'buffer',
+                -- 'ripgrep',
             },
+            -- providers = {
+            --     ripgrep = {
+            --         module = "blink-ripgrep",
+            --         name = "Ripgrep",
+            --         ---@module "blink-ripgrep"
+            --         ---@type blink-ripgrep.Options
+            --         opts = {
+            --             prefix_min_len = 3,
+            --             context_size = 5,
+            --             max_filesize = "1M",
+            --             project_root_marker = ".git",
+            --             project_root_fallback = true,
+            --             search_casing = "--ignore-case",
+            --             additional_rg_options = {},
+            --             fallback_to_regex_highlighting = true,
+            --             ignore_paths = {},
+            --             additional_paths = {},
+            --             toggles = {
+            --                 on_off = nil,
+            --             },
+            --             future_features = {
+            --                 backend = {
+            --                     use = "ripgrep",
+            --                 },
+            --             },
+            --             debug = false,
+            --         },
+            --     }
+            -- },
         },
         fuzzy = { implementation = "prefer_rust_with_warning" }
     },
